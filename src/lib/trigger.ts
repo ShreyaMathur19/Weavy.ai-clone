7bb01863c28715b35226c08d0f9c5a63257f6d0e
ShreyaMathur19 <Shreyamathur1984@gmail.com>
2026-02-13 23:33:14 +0530

    fix s

--- src/lib/trigger.ts
+++ src/lib/trigger.ts
@@ -1,6 +1,3 @@
-import { TriggerClient } from "@trigger.dev/sdk";
 
-export const trigger = new TriggerClient({
-  id: "workflow-builder",
-  apiKey: process.env.TRIGGER_API_KEY!,
-});
+
+export const trigger = {} as any;
